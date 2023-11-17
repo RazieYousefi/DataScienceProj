47db8ffaa838ab930fc9355ac002835bc78d0368
razie yousefi <razieyousefi@yahoo.com>
2023-11-17 08:32:29 -0600

    some minor changes

--- LinearReg/LinReg.py
+++ LinearReg/LinReg.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -12,7 +11,7 @@ from sklearn.datasets import fetch_california_housing
 housing = fetch_california_housing() # this is a class. 
 #to get to its attributes: print(dir(housing)) 
 # dir stands for directory.it is used to examin attributes of modules, classes and objects.
-print(dir(housing))
+print(f'housing attributes: {dir(housing)}')
 
 X = housing.data
 Y = housing.target
@@ -28,6 +27,12 @@ model = LinearRegression()
 #train the model
 model.fit(x_train, y_train)
 
+# get model parameters
+coef= model.coef_
+intercept = model.intercept_
+
+print (f'model coef: {coef} , and intercept: {intercept}')
+
 #make prediction on the test set
 y_pred = model.predict(x_test)
 
@@ -35,14 +40,38 @@ y_pred = model.predict(x_test)
 mse = mean_squared_error(y_test, y_pred)
 print(f'mean squared error: {mse}')
 
-
 #plot the results
 plt.scatter(y_test, y_pred)
 plt.xlabel("Actual Prices")
 plt.ylabel("Predicted Prices")
 plt.show()
 
+# you can use cross-validation to assess the model's performance more robustly
+from sklearn.model_selection import cross_val_score
 
+#perform cross validation
+cv_scores = cross_val_score(model, X, Y, cv=5, scoring='neg_mean_squared_error')
+cv_rmse_scores = np.sqrt(cv_scores)
+
+# Print cross-validation scores
+print("Cross-Validation RMSE Scores:", cv_rmse_scores)
+print("Mean RMSE:", np.mean(cv_rmse_scores))
+print("Standard Deviation of RMSE:", np.std(cv_rmse_scores))
+
+# Plot the data points
+plt.scatter(X_test[:, 0], y_test, color='blue', label='Actual Prices')
+
+# Plot the regression line
+x_line = np.linspace(min(X_test[:, 0]), max(X_test[:, 0]), 100).reshape(-1, 1)
+y_line = model.predict(x_line)
+plt.plot(x_line, y_line, color='red', linewidth=3, label='Linear Regression Line')
+
+# Add labels and legend
+plt.xlabel("Feature 1")
+plt.ylabel("House Price")
+plt.title("Linear Regression Model")
+plt.legend()
+plt.show()
 
 
 
